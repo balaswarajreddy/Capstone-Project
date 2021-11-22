3b17cb73abd84fa90897bc633857c78e2f97cccf
Bala Swaraj Reddy <40594778+balaswarajreddy@users.noreply.github.com>
2021-11-21 23:07:09 -0500

    Add files via upload

---
+++ app/routes/user.route.js
@@ -0,0 +1,21 @@
+module.exports = (app) => {
+    const users = require('../controllers/user.controller.js');
+    const storage = require('../helpers/storage');
+
+    // Create
+    app.post('/users', users.create);
+
+    // Create
+    app.post('/contacts', users.createContact);
+
+    // Retrieve all
+    app.get('/users', users.findAll);
+
+    //login check
+    app.post('/login', users.loginChecker);
+    
+    // Retrieve a single 
+    app.get('/users/:userId', users.findOne);
+
+    app.post('/user/:userId',storage, users.update);
+}
---
+++ config/database.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url:
+    
+    "mongodb+srv://brajesh:Rajesh93201@cluster0.nz5b2.mongodb.net/falmingo?retryWrites=true&w=majority",
+};
